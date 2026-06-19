dda2f85f53db0fce727956a496d592e7e3ea5127
PRASAD PANIGRAHY <prasadpanigrahy.ccschool@gmail.com>
2026-06-19 23:08:41 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,11 +6,11 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 CHROMA_DB_DIR = "./chroma_db"
 
-# Model Declarations
+
 GENERATION_MODEL = "gemini-2.5-flash"
 EMBEDDING_MODEL = "gemini-embedding-001"
 
-# Application Hard Thresholds
+
 RETRIEVAL_CONFIDENCE_THRESHOLD = 0.45
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
